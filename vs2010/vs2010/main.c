b1703aafcbd9fffca02a6f833c6e7613d96e8227
KimHyunJung <dnflchfhrdl@naver.com>
2017-06-01 21:00:50 +0900

    translated Ita->Eng

--- vs2010/vs2010/main.c
+++ vs2010/vs2010/main.c
@@ -57,6 +57,7 @@ void set_color(short Color)
   HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE); // Funzione che mi permette di cambiare colore del testo;
   SetConsoleTextAttribute(hCon,Color);
 }
+
 void boss(int vet[dim][dim],int dif)
 {
     int i,l,num,flag,k,spara;
@@ -228,6 +229,7 @@ void boss(int vet[dim][dim],int dif)
 
 
 }
+
 void ia_boss2(int vet[dim][dim],int dif)
 {
     int i,l,num,flag,k,spara;
@@ -789,16 +791,19 @@ void artificial_intelligence (int vet[dim][dim],int dif)
 
 }
 
-int condizione_vittoria(int vet[dim][dim])            // Funzione per verificare la condizione di vittoria o sconfitta del giocatore
+int condizione_vittoria(int vet[dim][dim])            
+// Function to verify the player's win or defeat condition
 {
-    int i,l,flag=0,giocatore=0;                         // inizializzo variabili
+    int i,l,flag=0,giocatore=0;                         // Variable initialization
 
     for(i=0;i<dim;i++){
         for(l=0;l<dim;l++){
-            if(vet[i][l]==3||vet[i][l]==7||vet[i][l]==10||vet[i][l]==12||vet[i][l]==14){                           //cerco la presenza sia del giocatore che dei nemici sul campo di gioco
-                flag=1;                                 // c'è un flag se trovo un nemico
+            if(vet[i][l]==3||vet[i][l]==7||vet[i][l]==10||vet[i][l]==12||vet[i][l]==14){                          
+  // I seek the presence of both the player and the enemy on the playing field
+                flag=1; 					// There is a flag if I find an enemy
+
             }if(vet[i][l]==2){
-                giocatore=1;                            // un altro flag se trovo il giocatore
+                giocatore=1; 				// Another flag if I find the player
             }
         }
     }
@@ -806,7 +811,8 @@ int condizione_vittoria(int vet[dim][dim])            // Funzione per verificare
     if(flag==1&&giocatore==1)
     {
         return 0;
-    }else if(flag==0&&giocatore==1){                   //Secondo chi è stato trovato sul campo la funzione restituisce valori differenti
+    }else if(flag==0&&giocatore==1){                 
+// According to who was found on the field, the function returns different values
         return 1;
     }else if(flag==1&&giocatore==0){
         return 2;
