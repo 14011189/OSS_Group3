449150bc1009d23662767416a8601667f0477342
KimHyunJung <dnflchfhrdl@naver.com>
2017-06-02 17:51:29 +0900

    added files about screen

---
+++ vs2010/vs2010/screen.c
@@ -0,0 +1,120 @@
+#include <windows.h>
+#include "screen.h"
+
+
+
+void sleep_mil(unsigned int ms)
+{
+    #if defined(WIN32)
+      Sleep(ms);
+    #elif defined(__unix__)
+      usleep(1000*ms);
+    #else
+      printf("Unsupported operating system!");
+    #endif
+
+}
+
+void clear_screen ( void )
+{
+  DWORD n;                         /* Number of characters written */
+  DWORD size;                      /* number of visible characters */
+  COORD coord = {0};               /* Top left screen position */
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+  /* Get a handle to the console */
+  HANDLE h = GetStdHandle ( STD_OUTPUT_HANDLE );
+
+  GetConsoleScreenBufferInfo ( h, &csbi );
+
+  /* Find the number of characters to overwrite */
+  size = csbi.dwSize.X * csbi.dwSize.Y;
+
+  /* Overwrite the screen buffer with whitespace */
+  FillConsoleOutputCharacter ( h, TEXT ( ' ' ), size, coord, &n );
+  GetConsoleScreenBufferInfo ( h, &csbi );
+  FillConsoleOutputAttribute ( h, csbi.wAttributes, size, coord, &n );
+
+  /* Reset the cursor to the top left position */
+  SetConsoleCursorPosition ( h, coord );
+}
+
+void set_color(short Color)
+{
+  HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE); // Funzione che mi permette di cambiare colore del testo;
+  SetConsoleTextAttribute(hCon,Color);
+}
+
+void disegna_schermo(int vet[dim][dim],int punt,int vit,int lv)                   // Funzione che disegna il campo di gioco da un vettore[n][n]
+{
+    int i,l;
+
+    printf(" vite: %d       (%d)     punteggio: %d",vit,lv,punt);
+
+    for(i=0;i<dim;i++){
+            printf("\n");
+        for(l=0;l<dim;l++){
+
+            if(i==0){
+                printf(" -");                             //secondo quali valori trova nel vettore sa se disegnare il giocatore, i nemici o gli altri aspetti grafici
+            }else if(i==dim-1){
+                printf(" -");
+            }else if(l==0){
+                printf(" | ");
+            }else if(l==dim-1){
+                printf(" | ");
+            }
+            else if(vet[i][l]==0){
+                printf("  ");
+            }else if(vet[i][l]==1){
+                printf(" |");
+            }else if(vet[i][l]==2){
+                set_color(11);
+                printf(" A");
+                set_color(15);
+            }else if(vet[i][l]==3){
+                set_color(14);
+                printf(" W");
+                set_color(15);
+            }else if(vet[i][l]==4){
+                set_color(12);
+                printf(" @");
+                set_color(15);
+            }else if(vet[i][l]==5){
+                set_color(6);
+                printf(" U");
+                set_color(15);
+            }else if(vet[i][l]==7){
+                set_color(2);
+                printf(" V");
+                set_color(15);
+            }else if(vet[i][l]==8){
+                set_color(10);
+                printf(" o");
+                set_color(15);
+            }else if(vet[i][l]==10){
+                set_color(3);
+                printf(" Y");
+                set_color(15);
+            }else if(vet[i][l]==12){
+                set_color(4);
+                printf(" W");
+                set_color(15);
+            }else if(vet[i][l]==14){
+                set_color(5);
+                printf(" X");
+                set_color(15);
+            }else if(vet[i][l]==15){
+                set_color(14);
+                printf(" *");
+                set_color(15);
+            }else if(vet[i][l]==16){
+                set_color(12);
+                printf(" @");
+                set_color(15);
+            }
+        }
+    }
+
+
+}
